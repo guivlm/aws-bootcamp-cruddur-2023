67f7e92f9470368d3bf0c1de3eea1db983b4c4e4
Guilherme Monteiro <guivlm.sp@gmail.com>
2023-03-08 15:07:11 +0000

    week-3-tasks(JWT Not Working)

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,6 +15,8 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
 # Honeycomb -----------
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -29,9 +31,9 @@ from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 # CloudWatch Logs -----------
-#import watchtower
-#import logging
-#from time import strftime
+# import watchtower
+# import logging
+# from time import strftime
 
 # Rollbar  -----------
 import rollbar
@@ -65,8 +67,15 @@ provider.add_span_processor(simple_processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
-# X-RAY
 app = Flask(__name__)
+
+cognito_jwt_token = CognitoJwtToken(
+    user_pool_id=os.getenv("AWS_COGNITO_USER_POLL_ID"),
+    user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+    region=os.getenv("AWS_DEFAULT_REGION")
+)
+
+# X-RAY
 XRayMiddleware(app, xray_recorder)
 
 # Honeycomb
@@ -77,6 +86,7 @@ RequestsInstrumentor().instrument()
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
+
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
@@ -84,7 +94,6 @@ cors = CORS(
   expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
-
 
 # @app.after_request
 # def after_request(response):
@@ -95,6 +104,8 @@ cors = CORS(
 
 # Rollbar  -----------
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+
+
 @app.before_first_request
 def init_rollbar():
     """init rollbar module"""
@@ -111,10 +122,12 @@ def init_rollbar():
     # send exceptions from `app` to rollbar, using flask's signal system.
     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
+
 @app.route('/rollbar/test')
 def rollbar_test():
     rollbar.report_message('Hello World!', 'warning')
     return "Hello World!"
+
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -157,13 +170,22 @@ def data_create_message():
 
 
 @app.route("/api/activities/home", methods=['GET'])
+# @aws_auth.authentication_required
 def data_home():
-    # data = HomeActivities.run(logger=LOGGER)
-    app.logger.debug('AUTH HEADER ------')
-    app.logger.debug(
-        request.headers.get('Authorization')
-        )
-    data = HomeActivities.run()
+    access_token = extract_access_token(request.headers)
+    try:
+        claims = cognito_jwt_token.verify(access_token)
+        # authenticated request
+        app.logger.debug('authenticated')
+        app.logger.debug(claims)
+        app.logger.debug(claims['username'])
+        # data = HomeActivities.run()
+        data = HomeActivities.run(cognito_user_id=claims['username'])
+    except TokenVerifyError as e:
+        # unauthenticated request
+        app.logger.debug('unauthenticated')
+        app.logger.debug(e)
+        data = HomeActivities.run()
     return data, 200
 
 
