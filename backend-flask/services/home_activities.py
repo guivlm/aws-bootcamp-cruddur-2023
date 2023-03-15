d963b2e4d3228f51184422ba7faf25f449987740
Guilherme Monteiro <guivlm.sp@gmail.com>
2023-03-15 09:02:18 +0000

    week-4-tasks (refactor db library)

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 # from opentelemetry import trace
 
-from lib.db import pool, query_wrap_array
+from lib.db import db
 
 # tracer = trace.get_tracer("home.activities")
 
@@ -16,34 +16,20 @@ class HomeActivities:
         #     now = datetime.now(timezone.utc).astimezone()
         #     span.set_attribute("app.now", now.isoformat())
 
-        sql = query_wrap_array("""
-        SELECT
-            activities.uuid,
-            users.display_name,
-            users.handle,
-            activities.message,
-            activities.replies_count,
-            activities.reposts_count,
-            activities.likes_count,
-            activities.reply_to_activity_uuid,
-            activities.expires_at,
-            activities.created_at
-        FROM public.activities
-        LEFT JOIN public.users ON users.uuid = activities.user_uuid
-        ORDER BY activities.created_at DESC
-        """)
-
-        print("=====sql=====")
-        print(sql)
-        print("=====sql=====")
-
-        with pool.connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(sql)
-                # this will return a tuple
-                    # the first field being the data
-                json = cur.fetchone()
-                print("----------------------")        
-                print(json)
-        return json[0]
+        results = db.query_array_json("""
+            SELECT
+                activities.uuid,
+                users.display_name,
+                users.handle,
+                activities.message,
+                activities.replies_count,
+                activities.reposts_count,
+                activities.likes_count,
+                activities.reply_to_activity_uuid,
+                activities.expires_at,
+                activities.created_at
+            FROM public.activities
+            LEFT JOIN public.users ON users.uuid = activities.user_uuid
+            ORDER BY activities.created_at DESC
+            """)
         return results
